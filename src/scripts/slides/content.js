23f1b4be35fd3fa6a469c31de62d1bab6ea2582d
Colin Gourlay <colin@colin-gourlay.com>
2015-05-21 01:55:47 +1000

    Added clock example

--- src/scripts/slides/content.js
+++ src/scripts/slides/content.js
@@ -1,4 +1,5 @@
 module.exports = [`
+
 <h1>Elements of Mercury</h1>
 <p>Colin Gourlay (<a href="http://twitter.com/collypops" target="_blank">@collypops</a>)</p>
 <br>
@@ -6,11 +7,7 @@ module.exports = [`
 
 `,`
 
-<p>one</p>
-
-`,`
-
-<h2>Click Counter</h2>
+<h4>Counter</h4>
 <pre>
 hg.state({
     counter: hg.value(0),
@@ -28,9 +25,52 @@ function render(state) {
 }
 </pre>
 
-`, {ClickCounter: `
+`, {Counter: `
 
-<p>Demo</p>
+<h4>Counter Demo</h4>
+
+`},`
+
+<h4>Clock</h4>
+<pre>
+hg.state({
+    time: hg.value(new Date()),
+    sync: hg.value(null),
+    channels: {
+        toggleSync: function (state) {
+            if (state.sync() != null) {
+                clearInterval(state.sync());
+                state.sync.set(null);
+            } else {
+                state.sync.set(setInterval(function () {
+                    state.time.set(new Date())
+                }, 1000));
+            }
+        }
+    }
+}
+</pre>
+
+`,`
+
+<h4>Clock (cont.)</h4>
+<pre>
+function render(state) {
+    return h('div', [
+        h('h4', state.time.toTimeString().split(' ')[0]),
+        h('button', {
+            style: {
+                'background-color': state.sync ? '#966' : '#696'
+            },
+            'ev-click': hg.sendClick(state.channels.toggleSync)
+        }, state.sync ? 'stop syncing' : 'start syncing')
+    ]);
+}
+</pre>
+
+`, {Clock: `
+
+<h4>Clock Demo</h4>
 
 `},`
 
--- src/scripts/slides/demos.js
+++ src/scripts/slides/demos.js
@@ -2,7 +2,7 @@ var hg = require('mercury');
 var h = require('mercury').h;
 
 var demos = {
-    ClickCounter: {
+    Counter: {
         state: hg.state({
             counter: hg.value(0),
             channels: {
@@ -14,7 +14,36 @@ var demos = {
         partial: function (state) {
             return h('button', {
                 'ev-click': hg.sendClick(state.channels.increment)
-            }, 'Clicks: ' + state.counter);
+            }, state.counter + ' clicks');
+        }
+    },
+    Clock: {
+        state: hg.state({
+            time: hg.value(new Date()),
+            sync: hg.value(null),
+            channels: {
+                toggleSync: function (state) {
+                    if (state.sync() != null) {
+                        clearInterval(state.sync());
+                        state.sync.set(null);
+                    } else {
+                        state.sync.set(setInterval(function () {
+                            state.time.set(new Date())
+                        }, 1000));
+                    }
+                }
+            }
+        }),
+        partial: function (state) {
+            return h('div', [
+                h('h4', state.time.toTimeString().split(' ')[0]),
+                h('button', {
+                    style: {
+                        'background-color': state.sync ? '#966' : '#696'
+                    },
+                    'ev-click': hg.sendClick(state.channels.toggleSync)
+                }, state.sync ? 'stop syncing' : 'start syncing')
+            ]);
         }
     }
 };
